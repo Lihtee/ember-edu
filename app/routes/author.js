d7210311d841f3a2243a73d33e0aa8bebb8124fe
unknown <johndoe@example.com>
2019-07-16 20:51:47 +0300

    Added search.

--- app/routes/author.js
+++ app/routes/author.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-    model(){
+    queryParams: {
+        search: {
+            refreshModel: true
+        }
+    },
+
+    model({ search }){
+        if (search){
+            // /authors?filter[query]=a
+            return this.store.query('author', {
+                filter: {
+                    query: search
+                }
+            });
+        }
+
         return this.store.findAll('author')
     }
 });
